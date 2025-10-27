d2d1ab69bf8a5026b74a584178077854127e4312
baloogedit <baloogedit@gmail.com>
2025-10-27 10:54:07 +0200

    added abstract class

---
+++ src/main/java/umfst/oop/tancprojekt/Person.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umfst.oop.tancprojekt;
+
+/**
+ *
+ * @author edite
+ */
+public abstract class Person {
+    
+    protected String name;
+    protected int age;
+    
+    public Person(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    public abstract void intro();
+    
+    
+    
+}
