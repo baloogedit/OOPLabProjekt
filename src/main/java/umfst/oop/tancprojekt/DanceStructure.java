07e83c77fc8ad4ab43ed3973a47df2dcaa96e12f
baloogedit <baloogedit@gmail.com>
2025-10-29 14:24:05 +0200

    add interface

---
+++ src/main/java/umfst/oop/tancprojekt/DanceStructure.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package umfst.oop.tancprojekt;
+
+/**
+ *
+ * @author edite
+ */
+public interface DanceStructure {
+    
+    //start performing the dance
+    void perform();
+    
+    //show the dance's origin or region
+    void showOrigin();
+    
+    //display how long the dance lasts
+    void showDuration();
+    
+    // display full details of the dance
+    void print();
+    
+    // describe what type of dance it is
+    void describeStyle();
+}
