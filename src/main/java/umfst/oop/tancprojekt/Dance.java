cceb326be40802d15b5dee5cc7c7e3801c625204
baloogedit <baloogedit@gmail.com>
2025-10-29 14:24:27 +0200

    add implementation of interface

--- src/main/java/umfst/oop/tancprojekt/Dance.java
+++ src/main/java/umfst/oop/tancprojekt/Dance.java
@@ -8,7 +8,7 @@ package umfst.oop.tancprojekt;
  *
  * @author edite
  */
-public class Dance {
+public class Dance implements DanceStructure{
     
     public String danceName;
     public String region;
@@ -20,10 +20,41 @@ public class Dance {
         this.minutes = minutes;
     }
     
+    @Override
     public void print()
     {
         System.out.println("Dance: " +danceName+" from: " +region+ ", duration: " +minutes+ " minutes." );
     }
+    
+    @Override
+    public void perform() {
+        System.out.println("Performing the dance '" + danceName + "' energetically on stage!");
+    }
+
+    @Override
+    public void showOrigin() {
+        System.out.println("This dance originates from the region of " + region + ".");
+    }
+
+    @Override
+    public void showDuration() {
+        System.out.println("The dance '" + danceName + "' lasts about " + minutes + " minutes.");
+    }
+
+    @Override
+    public void describeStyle() {
+        if (minutes < 3) {
+            System.out.println(danceName + " is a short, lively dance.");
+        } else if (minutes < 7) {
+            System.out.println(danceName + " is a traditional medium-length folk dance.");
+        } else {
+            System.out.println(danceName + " is a long ceremonial performance.");
+        }
+    }
+    
+    
+    
+    
     
 
     public String getName() {
